755edb695abd85b47db5d5b9f39b6d49f1e15c58
“Daysha <+>
2022-03-02 11:44:35 -0800

    added character model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_190445) do
+ActiveRecord::Schema.define(version: 2022_03_02_193803) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "thumbnail"
+    t.integer "team_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
